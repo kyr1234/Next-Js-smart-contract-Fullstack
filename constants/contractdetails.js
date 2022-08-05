1b2eb556f88fba07a0c09253c1c440bbd774b4e1
kyr1234 <yugkhokhar18@gmail.com>
2022-08-06 00:15:30 +0530

    ADDED SOMEPART OF CHANGES

--- constants/contractdetails.js
+++
@@ -1,6 +0,0 @@
-const contractaddress=require("./contractaddress.json")
-const abi = require("./abi.json")
-
-module.exports = {
-    contractaddress,abi
-}
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,29 @@
 import { MoralisProvider } from 'react-moralis'
 
 import { NotificationProvider } from 'web3uikit'
+import Head from 'next/head'
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <NotificationProvider>
-        <Component {...pageProps} />
-      </NotificationProvider>
-    </MoralisProvider>
+    <div className="">
+      <Head>
+        <title>NFT-MARKETPLACE</title>
+        <meta name="description" content="NFT MARKETPLACE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider
+        initializeOnMount={true}
+        appId={APP_ID}
+        serverUrl={SERVER_URL}
+      >
+        <NotificationProvider>
+          <Component {...pageProps} />
+        </NotificationProvider>
+      </MoralisProvider>
+    </div>
   )
 }
 
