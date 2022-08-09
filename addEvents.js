3de8b6e3d0c777f9dcb8c822f5abea01b4a76eb7
kyr1234 <yugkhokhar18@gmail.com>
2022-08-09 20:25:51 +0530

    ADDED CLOUD FUNCTIONS

--- addEvents.js
+++ addEvents.js
@@ -2,7 +2,7 @@ const Moralis = require('moralis/node')
 require('dotenv').config()
 const contractAddresses = require('./constants/contract.json')
 let chainId = process.env.chainId || 31337
-let moralischainid = chainId == '31337' ? '1337' : chainId
+let moralisChainId = chainId == '31337' ? '1337' : chainId
 const contractAddressArray = contractAddresses[chainId]['NftMarketplace']
 const contractAddress = contractAddressArray[contractAddressArray.length - 1]
 
@@ -14,60 +14,15 @@ async function main() {
   await Moralis.start({ serverUrl, appId, masterKey })
   console.log(`Working with contrat address ${contractAddress}`)
 
-  const itemListed = {
-    chainId: moralischainid,
+  let itemListedOptions = {
+    // Moralis understands a local chain is 1337
+    chainId: moralisChainId,
     sync_historical: true,
-    topic: 'Item_Listed(address,uint256,uint256,,address)',
+    topic: 'ItemListed(address,address,uint256,uint256)',
     address: contractAddress,
     abi: {
       anonymous: false,
       inputs: [
-        {
-          indexed: true,
-          internalType: 'address',
-          name: 'nftaddress',
-          type: 'address',
-        },
-        {
-          indexed: true,
-          internalType: 'uint256',
-          name: 'tokenid',
-          type: 'uint256',
-        },
-        {
-          indexed: false,
-          internalType: 'uint256',
-          name: 'price',
-          type: 'uint256',
-        },
-        {
-          indexed: true,
-          internalType: 'address',
-          name: 'owner',
-          type: 'address',
-        },
-      ],
-      name: 'Item_Listed',
-      type: 'event',
-    },
-    limit: 500000,
-    tableName: 'ITEMLISTED',
-  }
-
-  const Items_Bought = {
-    chainId: moralischainid,
-    sync_historical: true,
-    topic: 'Item_Bought_OwnerShip_Transfer(address,address,uint256,uint256)',
-    address: contractAddress,
-    abi: {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: true,
-          internalType: 'address',
-          name: 'nftaddress',
-          type: 'address',
-        },
         {
           indexed: true,
           internalType: 'address',
@@ -76,8 +31,14 @@ async function main() {
         },
         {
           indexed: true,
+          internalType: 'address',
+          name: 'nftAddress',
+          type: 'address',
+        },
+        {
+          indexed: true,
           internalType: 'uint256',
-          name: 'tokenid',
+          name: 'tokenId',
           type: 'uint256',
         },
         {
@@ -87,71 +48,105 @@ async function main() {
           type: 'uint256',
         },
       ],
-      name: 'Item_Bought_OwnerShip_Transfer',
+      name: 'ItemListed',
       type: 'event',
     },
-    limit: 500000,
-    tableName: 'ITEMSBOUGHT',
+    tableName: 'ItemListed',
   }
 
-  const Cancel_Listed = {
-    chainId: moralischainid,
-    sync_historical: true,
-    topic: 'Cancel_Listed(address,uint256,address)',
+  let itemBoughtOptions = {
+    chainId: moralisChainId,
     address: contractAddress,
+    sync_historical: true,
+    topic: 'ItemBought(address,address,uint256,uint256)',
     abi: {
       anonymous: false,
       inputs: [
         {
           indexed: true,
           internalType: 'address',
-          name: 'nftaddress',
+          name: 'buyer',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'nftAddress',
           type: 'address',
         },
         {
           indexed: true,
           internalType: 'uint256',
-          name: 'tokenid',
+          name: 'tokenId',
           type: 'uint256',
         },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'price',
+          type: 'uint256',
+        },
+      ],
+      name: 'ItemBought',
+      type: 'event',
+    },
+    tableName: 'ItemBought',
+  }
+
+  let itemCanceledOptions = {
+    chainId: moralisChainId,
+    address: contractAddress,
+    topic: 'ItemCanceled(address,address,uint256)',
+    sync_historical: true,
+    abi: {
+      anonymous: false,
+      inputs: [
         {
           indexed: true,
           internalType: 'address',
           name: 'seller',
           type: 'address',
         },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'nftAddress',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'uint256',
+          name: 'tokenId',
+          type: 'uint256',
+        },
       ],
-      name: 'Cancel_Listed',
+      name: 'ItemCanceled',
       type: 'event',
     },
-    tableName: 'ITEMSCANCEL',
-    limit: 500000,
+    tableName: 'ItemCanceled',
   }
 
   const listedResponse = await Moralis.Cloud.run(
     'watchContractEvent',
-    itemListed,
+    itemListedOptions,
     {
       useMasterKey: true,
     },
   )
   const boughtResponse = await Moralis.Cloud.run(
     'watchContractEvent',
-    Items_Bought,
+    itemBoughtOptions,
     {
       useMasterKey: true,
     },
   )
   const canceledResponse = await Moralis.Cloud.run(
     'watchContractEvent',
-    Cancel_Listed,
+    itemCanceledOptions,
     {
       useMasterKey: true,
     },
   )
-  console.log(listedResponse)
-  console.log(canceledResponse.success)
-  console.log(boughtResponse.success)
   if (
     listedResponse.success &&
     canceledResponse.success &&
