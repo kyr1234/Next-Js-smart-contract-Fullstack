e518aea7039b40c68f96294df04dd3fc17351961
kyr1234 <yugkhokhar18@gmail.com>
2022-09-12 14:51:24 +0530

    THE FILES ADDED

--- cloud-functions/updateFunction.js
+++ cloud-functions/updateFunction.js
@@ -1,5 +1,4 @@
-/*market 0x5FbDB2315678afecb367f032d93F642f64180aa3 */
-/* basic 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 */
+
 Moralis.Cloud.afterSave("ItemListed", async (request) => {
     const confirmed = request.object.get("confirmed")
     const logger = Moralis.Cloud.getLogger()
